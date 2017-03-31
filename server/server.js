19ae90a8b7abcb8ff1a6b889df420c98d0af2930
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2017-03-31 11:03:31 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+const path = require('path');
+const express = require('express');
+
+var app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+})
