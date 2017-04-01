31f1073554a797be86ed111ebbc14124f59b719f
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2017-03-31 19:05:24 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@
 var socket = io();
 socket.on('connect', function ()  {
   console.log('Connected to server');
-  socket.emit('createMessage', {
-    from: 'Bill',
-    text: 'Hi, it\'s Bill'
-  });
 });
 
 
--- server/server.js
+++ server/server.js
@@ -16,18 +16,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Fionna',
-    text: 'Let\'s fuck',
-    createdAt: 1490979444
-  });
-
   socket.on('disconnect', () => {
     console.log('Disconnected from server');
   });
 
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 });
 
